8decf34ea5a596879cbed873f00b3ce972057029
Zhang Zongyu <zongyu@novazy.net>
2021-08-17 16:08:26 +0800

    Python: add interface for Whittaker–Shannon interpolation
    see: https://en.wikipedia.org/wiki/Whittaker%E2%80%93Shannon_interpolation_formula
    
    Signed-off-by: Zhang Zongyu <zongyu@novazy.net>

---
+++ Python/interp/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*-
+
+from .sinc import Sinc
---
+++ Python/interp/sinc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import numpy as np
+from functools import reduce
+
+class Sinc():
+
+    def __init__(self, data, sample_interval):
+        self.base_data = data
+        self.sample_interval = sample_interval
+
+    def __call__(self, t):
+        tmp = []
+        for i in range(self.base_data.size):
+            v = self.base_data[i] * np.sinc(t/self.sample_interval - i)
+            tmp.append(v)
+        return reduce(lambda a, b: a+b, tmp)
